b6d306fa80d8555d2f9cbe22811c06c789f42866
zrokhit <zrokhit@uwaterloo.ca>
2025-10-21 17:51:11 +0100

    Created app file structure and barebones express server

---
+++ apps/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
