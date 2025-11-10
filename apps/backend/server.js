d29d8b8dc5d25a717b3c5ffb9570002800feaf53
zrokhit <zrokhit@uwaterloo.ca>
2025-11-10 08:41:36 +0000

    created general frame of all backend apis, will fill in logic in next commit

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const session = require("express-session");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
@@ -9,6 +9,48 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/feed", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/like", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/dislike", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/create-playlist", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/modify-playlist", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/delete-playlist", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/publish-playlist", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/add-song", (req, res) => {
+  res.send("Hello World!");
+});
+
+// TODO: Let's treat auth feature as a 'nice to have' we only add if time permits
+
+// app.post("/login", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.post("/signup", (req, res) => {
+//   res.send("Hello World!");
+// });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`The Queue listening on port ${port}`);
 });
