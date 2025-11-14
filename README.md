157109610d34a13483b6f33c8366ae0897df5d77
espiidev <chloexr06@gmail.com>
2025-11-14 13:35:23 +0000

    Implemented main page routing

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A platform to post, create and share your favourite playlists!
 
 link to the figma design - https://www.figma.com/design/6cJWRVJIHfD2BujBlbXx1q/Web-Design-Project---The-Queue?node-id=0-1&t=DXV4vu0iq8Z7Xw5z-1
+
+to run the app on the front end use npm run dev in the directory apps/frontend/the-queue
--- apps/frontend/the-queue/src/App.jsx
+++ apps/frontend/the-queue/src/App.jsx
@@ -1,33 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import homeIcon from './assets/home.svg';
+import playlistIcon from './assets/playlist.svg';
+import accountIcon from './assets/account.svg';
+import logo from './assets/logo.png';
+
+const navLinkStyles = ({ isActive }) => ({
+    backgroundColor: isActive ? '#F4C3A4' : 'transparent',
+    borderWidth: isActive ? '2px 0px 2px 2px' : "2px 0px 2px 2px",
+    borderStyle: 'solid',
+    borderColor: isActive ? '#000000' : 'transparent',
+})
+
+function Home() {
+    return <h1>home test</h1>
+}
+function Playlist() {
+    return <h1>playlist test</h1>
+}
+function Account() {
+    return <h1>account test</h1>
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="main"> {/* main container */}
+        <BrowserRouter>
+
+          <nav>                                           {/*side navigation bar - used for routing with NavLink and specified routes */}                                    
+            <img src={logo} alt="The Queue Logo" />       {/* calling components with return into the feed element*/}
+            <NavLink to = "/" style={navLinkStyles}>
+              <img src={homeIcon} alt="home icon"/>
+              Home    
+            </NavLink>
+            <NavLink to = "/create-playlist" style={navLinkStyles}>
+              <img src={playlistIcon} alt="playlist icon"/>
+              Create Playlist  
+            </NavLink>
+            <NavLink to = "/account" style={navLinkStyles}>
+              <img src={accountIcon} alt="account icon"/>
+              Account  
+            </NavLink>
+          </nav> 
+
+          <div id="feed">
+            <Routes>
+              <Route path='/' element = {<Home />} ></Route>
+              <Route path='/create-playlist' element = {<Playlist />} ></Route>
+              <Route path='/account' element = {<Account />}> </Route> 
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
