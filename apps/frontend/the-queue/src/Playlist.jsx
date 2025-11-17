2e0177a1bb2487dac07d3713fb79e609c051b7e6
Alexander310101 <alexander.muir.67@outlook.com>
2025-11-17 16:13:28 +0000

    dasf

--- apps/frontend/the-queue/src/Playlist.jsx
+++ apps/frontend/the-queue/src/Playlist.jsx
@@ -2,32 +2,19 @@ import Post from "./Post";
 
 export default function Playlist(props) {
 
-    
-
-
-
-
-
-
-    
-
   return <div id="playlistCreator">
-    <form>
       <label>Enter playlist Name:
         <input type="text" />
-      </label>
+    </label>
+        <div id="search-songs">
+
       <label>Enter The Name of the song you wish to add:
         <input type="text" />
       </label>
-    </form>
-
+        <div className="dropdown">ygfdgdfsgfdsgtyu</div>
+        </div>
 
 
 
   </div>
-
-
-
-
-  ;
 }
